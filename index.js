d69f2510c88d83af5491097a04dabfc54e34133a
davidnene <davienesh4@gmail.com>
2022-06-10 11:58:19 +0300

    commit tree 59755ce147bf9a4bb06276ff44e81ea3990e7940

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ];
+
+/*
+function superbowlWin(record) {
+    if(record.result === 'W') {
+        return record.year }
+    
+}
+*/
+
+function superbowlWin(record) {
+    const ans = record.find(record => record.result === 'W')
+        return ans ? ans.year : undefined
+       
+}
